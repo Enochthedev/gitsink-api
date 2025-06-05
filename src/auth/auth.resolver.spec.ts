baae23bff132455d990320443b410ea4d3b0e375
Wave <enochjesse884@gmail.com>
2025-06-05 13:28:33 +0100

    feat: use stored GitHub tokens

--- src/auth/auth.resolver.spec.ts
+++ src/auth/auth.resolver.spec.ts
@@ -2,13 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('AuthResolver', () => {
   let resolver: AuthResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthResolver, AuthService, PrismaService],
+      providers: [AuthResolver, AuthService, PrismaService, ConfigService],
     }).compile();
 
     resolver = module.get<AuthResolver>(AuthResolver);
