959b925879a54903d1bf9c58b90e3e140f62c7c8
Wave <enochjesse884@gmail.com>
2025-06-05 13:53:12 +0100

    Fix tests for project services

--- src/projects/projects.service.spec.ts
+++ src/projects/projects.service.spec.ts
@@ -10,15 +10,8 @@ describe('ProjectsService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProjectsService, PrismaService, ParserService, ConfigService],
-
       imports: [CacheModule.register()],
-      providers: [
-        ProjectsService,
-        PrismaService,
-        ParserService,
-        ConfigService,
-      ],
+      providers: [ProjectsService, PrismaService, ParserService, ConfigService],
     }).compile();
 
     service = module.get<ProjectsService>(ProjectsService);
