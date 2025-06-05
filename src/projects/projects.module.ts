563fcdf15a1f4ba86a9d920a911b163057dbf23e
Wave <enochjesse884@gmail.com>
2025-06-05 13:19:28 +0100

    Add API key hashing and guard

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -5,8 +5,9 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { ParserModule } from '../parser/parser.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProjectFieldsResolver } from './project-fields.resolver';
+import { ApiKeyAuthGuard } from '../auth/api-key-auth.guard';
 @Module({
   imports: [PrismaModule, ParserModule, ConfigModule],
-  providers: [ProjectsService, ProjectsResolver, ProjectFieldsResolver],
+  providers: [ProjectsService, ProjectsResolver, ProjectFieldsResolver, ApiKeyAuthGuard],
 })
 export class ProjectsModule {}
