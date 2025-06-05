1e60a463ac39122b9d032a7fc11ca77d0ba11216
Wave <enochjesse884@gmail.com>
2025-06-05 11:28:23 +0100

    fix tests and config

--- src/projects/projects.service.spec.ts
+++ src/projects/projects.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectsService } from './projects.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { ParserService } from '../parser/parser.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('ProjectsService', () => {
   let service: ProjectsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProjectsService],
+      providers: [
+        ProjectsService,
+        PrismaService,
+        ParserService,
+        ConfigService,
+      ],
     }).compile();
 
     service = module.get<ProjectsService>(ProjectsService);
