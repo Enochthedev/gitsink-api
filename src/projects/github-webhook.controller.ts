37b48264f4c2ccf8f53e3740cf754355ade065b0
Wave <enochjesse884@gmail.com>
2025-06-06 14:34:51 +0100

    Add Swagger documentation

--- src/projects/github-webhook.controller.ts
+++ src/projects/github-webhook.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ProjectsService } from './projects.service';
 import { ConfigService } from '@nestjs/config';
 
+@ApiTags('webhook')
 @Controller('webhook/github')
 export class GitHubWebhookController {
   constructor(
